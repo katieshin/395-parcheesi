53de016150e1545d37cf827f4a5ae7661e51aa82
skorlir <skorlir@gmail.com>
2017-04-10 22:48:26 -0500

    BUGFIX: InvalidDieException must be public and instantiated

--- parcheesi/Die.java
+++ parcheesi/Die.java
@@ -1,17 +1,21 @@
 package parcheesi;
 
 public class Die {
-	class InvalidDieException extends Exception { }
+	public class InvalidDieException extends Exception {
+		public InvalidDieException(int value) {
+			super("Given invalid die roll: " + String.valueOf(value));
+		}
+	}
 
 	int value;
 
-	public Die(int value) {
+	public Die(int value) throws InvalidDieException {
 		if (value >= 1 && value <= 6) {
 			this.value = value;
 		} else {
 			// NOTE: Game will implode if invalid die roll is passed to constructor.
 			// FIXME: May not be the best UX?
-			throw InvalidDieException("Given invalid die roll: " + String.valueOf(value));
+			throw new InvalidDieException(value);
 		}
 	}
 
