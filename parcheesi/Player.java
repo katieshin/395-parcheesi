a9e9c74d0120470f47361deed76f40b57fb14569
skorlir <skorlir@gmail.com>
2017-04-11 02:51:00 -0500

    Correct Player interface
    * Return type of doMove should be Move[] (per website)
    * Needs imports

--- parcheesi/Player.java
+++ parcheesi/Player.java
@@ -1,14 +1,15 @@
 package parcheesi;
 
-// NOTE: Do not change this interface!
-interface Player {
+import parcheesi.move.Move;
 
+// NOTE: Do not change this interface!
+public interface Player {
 	// inform the player that a game has started
 	// and what color the player is.
 	void startGame(String color);
 
 	// ask the player what move they want to make
-	Move doMove(Board brd, int[] dice);
+	Move[] doMove(Board brd, int[] dice);
 
 	// inform the player that they have suffered
 	// a doubles penalty
