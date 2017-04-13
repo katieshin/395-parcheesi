7a83badb10b372e93fa262b22f79485226f0b5f3
skorlir <skorlir@gmail.com>
2017-04-12 21:05:41 -0500

    WIP spec for more rules in rule.Rule comments

--- parcheesi/rule/Rule.java
+++ parcheesi/rule/Rule.java
@@ -11,3 +11,66 @@ public interface Rule {
 	// moves is the stored result of player.doMove(...), and after is the resulting board.
 	public boolean enforce (Die[] dice, Player player, Board before, Move[] moves, Board after);
 }
+
+// public class Home extends LogicalAndRuleset {
+// 	/* NOTE: Rule is made redundant by other rules.
+// 	 * public class MustEnterByExactCount implements Rule {
+// 	 *	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 	 *		return false;
+// 	 *	}
+// 	 * }
+// 	 */
+
+// 	public class MustUseHomeBonus implements Rule {
+// 		public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 			return false;
+// 		}
+// 	}
+
+// 	// NOTE: All must be true relationship.
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return MustUseHomeBonus(moves, dice, before, after);
+// 	}
+// }
+
+// public class UseAllDice implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class NoSplittingDie implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class MoveCounterClockwise implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class CannotMovePastBlockade implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class SafetySquareOccupancy implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class CannotEnterOpponentHomeRow implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
+
+// public class MaximumSquareOccupancy implements Rule {
+// 	public boolean enforce (Move[] moves, Die[] dice, Board before, Board after) {
+// 		return false;
+// 	}
+// }
