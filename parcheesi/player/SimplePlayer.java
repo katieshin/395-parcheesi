39f6b9847c1351de76e468032c0a6be8875821a5
skorlir <skorlir@gmail.com>
2017-04-12 21:06:03 -0500

    WIP SimplePlayer Player implementation

---
+++ parcheesi/player/SimplePlayer.java
@@ -0,0 +1,41 @@
+package parcheesi.player;
+
+import parcheesi.move.Move;
+import parcheesi.die.Die;
+import parcheesi.Board;
+import parcheesi.Color;
+import parcheesi.Pawn;
+
+public class SimplePlayer implements Player {
+	int doublesRolled = 0;
+
+	Color color;
+
+	// TODO?
+	// NOTE: Stub
+	public SimplePlayer () { }
+
+	public void startGame(String color) {
+		this.color = Color.lookupByColorName(color);
+	}
+
+	public Move[] doMove(Board board, int[] dice) {
+		// TODO
+		return new Move[] {};
+	}
+
+	public void DoublesPenalty() {
+		doublesRolled = 0;
+		// TODO
+		// getFurthestPawn().restart();
+	}
+
+	public Pawn getFurthestPawn() {
+		// TODO
+		return new Pawn(3, "blue");
+	}
+
+	public Color getColor() {
+		return color;
+	}
+}
