ca90469a93357ccbac466f1189f89f9e8a877430
fluffywaffles <skorlir@gmail.com>
2017-04-28 00:30:11 -0500

    Fix move Manifest so that it actually compiles

--- parcheesi/move/Manifest.java
+++ parcheesi/move/Manifest.java
@@ -1,13 +1,21 @@
 package parcheesi.move;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /* NOTE: This would need to be updated with any new Moves if any new Moves were to be created.
  * TranslatedMove depends on its accuracy.
  */
 // TODO: Use Java's crazy-ass dynamic classloader stuff to generate this data automagically.
 public class Manifest {
-	public static Class<? extends Move> MOVE_CLASSES = {
-		MoveMain.class,
-		MoveHome.class,
-		EnterPiece.class
-	}
+	// NOTE: Cannot use a plain Array here if we want type-safety.
+	// See: http://stackoverflow.com/questions/3865946/error-generic-array-creation
+	public static final List<Class<? extends Move>> MOVE_CLASSES = new ArrayList<>(
+		Arrays.asList(
+			MoveMain.class,
+			MoveHome.class,
+			EnterPiece.class
+		)
+	);
 }
