e7d5b1c8479027127eca38e32298c4e67902cd21
fluffywaffles <skorlir@gmail.com>
2017-04-28 00:29:52 -0500

    Make all Parameters public

--- parcheesi/Parameters.java
+++ parcheesi/Parameters.java
@@ -1,53 +1,53 @@
 package parcheesi;
 
-class Parameters {
-	static final int maxPlayers = 4;
+public class Parameters {
+	public static final int maxPlayers = 4;
 
 	// Unlikely that we'll want to change this, but let's use a variable so that we can.
-	static final int pawnsPerPlayer = 4;
+	public static final int pawnsPerPlayer = 4;
 
 	// Board layout/size parameters.
-	class Board {
+	public class Board {
 		// Corresponds to number of players that can exist on the board.
-		static final int dimensions = maxPlayers;
+		public static final int dimensions = maxPlayers;
 		// NOTE: If dimension changes, renderer will need to be able to handle it.
 
 		// Cannot be odd (our later calculations will break).
-		static final int spacesPerRow = 8;
+		public static final int spacesPerRow = 8;
 
 		// Must be odd (in order to have a middle row for the home row).
 		// FIXME: Cannot actually adjust this value because *pattern* tests don't account for it.
-		static final int rowsPerDimension = 3;
+		public static final int rowsPerDimension = 3;
 
 		/* Corresponds to the distance between two nodes of alike type and same relative
 		 * position in adjacent dimensions.
 		 *   e.g.: dist(Entry in dimension 2, Entry in dimension 1) = dimensionSize
 		 */
-		static final int dimensionSize = spacesPerRow * rowsPerDimension;
+		public static final int dimensionSize = spacesPerRow * rowsPerDimension;
 
 		/* Corresponds to the distance between two nodes of alike type in two different dimensions,
 		 * not counting the home row (i.e.; main ring distance, so the distance pawns travel).
 		 *   e.g.: dist(Entry 1, Entry 2) for Player 1 Pawn = mainRingSizePerDimension
 		 */
-		static final int mainRingSizePerDimension = dimensionSize - (spacesPerRow - 1);
+		public static final int mainRingSizePerDimension = dimensionSize - (spacesPerRow - 1);
 
-		static final int size = dimensionSize * dimensions;
+		public static final int size = dimensionSize * dimensions;
 
 		// These are used as a convenience during Board generation.
 		// FIXME?: These calculations don't work correctly if spacesPerRow is odd.
 		// FIXME?: These calculations don't work correctly if rowsPerDimension is even.
-		static final int firstSafeIndex      = spacesPerRow / 2 - 1;
-		static final int firstHomeEntryIndex = spacesPerRow;
-		static final int firstEntryIndex     = firstHomeEntryIndex + spacesPerRow + (spacesPerRow / 2);
+		public static final int firstSafeIndex      = spacesPerRow / 2 - 1;
+		public static final int firstHomeEntryIndex = spacesPerRow;
+		public static final int firstEntryIndex     = firstHomeEntryIndex + spacesPerRow + (spacesPerRow / 2);
 
 		// Convenient aliases that have more meaning.
-		static final int safeIndexRelativeToDimensionStart = firstSafeIndex;
-		static final int homeEntryIndexRelativeToDimensionStart = firstHomeEntryIndex;
-		static final int entryIndexRelativeToDimensionStart = firstEntryIndex;
-		static final int spacesLeftAfterEntry = dimensionSize - entryIndexRelativeToDimensionStart;
+		public static final int safeIndexRelativeToDimensionStart = firstSafeIndex;
+		public static final int homeEntryIndexRelativeToDimensionStart = firstHomeEntryIndex;
+		public static final int entryIndexRelativeToDimensionStart = firstEntryIndex;
+		public static final int spacesLeftAfterEntry = dimensionSize - entryIndexRelativeToDimensionStart;
 
-		static final int pawnMainRingDistance = mainRingSizePerDimension * (dimensions - 1);
-		static final int maxPawnTravelDistance
+		public static final int pawnMainRingDistance = mainRingSizePerDimension * (dimensions - 1);
+		public static final int maxPawnTravelDistance
 			= spacesLeftAfterEntry
 				+ pawnMainRingDistance
 				+ homeEntryIndexRelativeToDimensionStart
