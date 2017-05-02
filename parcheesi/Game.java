b296c33bb0b8fa2218a61e912619b50622e65008
fluffywaffles <skorlir@gmail.com>
2017-05-02 13:38:39 -0500

    WIP Game

--- parcheesi/Game.java
+++ parcheesi/Game.java
@@ -1,5 +1,7 @@
 package parcheesi;
 
+import static parcheesi.Parameters;
+
 import parcheesi.player.Player;
 
 // NOTE: Do not change this interface!
@@ -9,4 +11,35 @@ interface Game {
 
 	// start a game
 	void start();
+}
+
+public class Parcheesi implements Game {
+	/*
+	 * Manage:
+	 *	Players
+	 *	Pawns
+	 *	Board
+	 *	Turns
+	 */
+	// Players and Pawns.
+	Player[] players = new Player[Parameters.maxPlayers];
+	Pawn[] pawns     = new Pawn[Parameters.pawnsPerPlayer * Parameters.maxPlayers];
+
+	// The current Board.
+	Board currentBoard = new Board();
+
+	// Complete history of the Game (by Turns).
+	List<Turn> turnHistory = new ArrayList<Turn>();
+
+	public Parcheesi() {
+
+	}
+
+	void register(Player p) {
+
+	}
+
+	void start() {
+
+	}
 }
