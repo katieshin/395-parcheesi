879ed08de6021cc2dfda9ada9c2e4c3005dd92eb
fluffywaffles <skorlir@gmail.com>
2017-05-04 15:06:44 -0500

    Just make pawn fields public already

--- parcheesi/pawn/Pawn.java
+++ parcheesi/pawn/Pawn.java
@@ -2,8 +2,8 @@ package parcheesi.pawn;
 
 // NOTE: Do not change this interface!
 public class Pawn {
-	int /* 0-3 */ id;
-	String color;
+	public int /* 0-3 */ id;
+	public String color;
 	public Pawn (int id, String color) {
 		this.id=id;
 		this.color=color;
