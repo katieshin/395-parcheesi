2494e0f72b9e4db3c1d46797b40567be879da933
fluffywaffles <skorlir@gmail.com>
2017-05-25 20:14:50 -0500

    Simplify Move manifest

--- parcheesi/move/Manifest.java
+++ parcheesi/move/Manifest.java
@@ -9,13 +9,9 @@ import java.util.List;
  */
 // TODO: Use Java's crazy-ass dynamic classloader stuff to generate this data automagically.
 public class Manifest {
-	// NOTE: Cannot use a plain Array here if we want type-safety.
-	// See: http://stackoverflow.com/questions/3865946/error-generic-array-creation
-	public static final List<Class<? extends Move>> MOVE_CLASSES = new ArrayList<>(
-		Arrays.asList(
-			MoveMain.class,
-			MoveHome.class,
-			EnterPiece.class
-		)
+	public static final List<Class<? extends Move>> MOVE_CLASSES = Arrays.asList(
+		MoveMain.class,
+		MoveHome.class,
+		EnterPiece.class
 	);
 }
