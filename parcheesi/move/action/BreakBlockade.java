5755a80d0c8fcd71e92e4108db6ccdd95776ee08
fluffywaffles <skorlir@gmail.com>
2017-05-26 19:18:25 -0500

    Progress on BreakBlockade and FormBlockade
    * isApplicable
    * Tests for isApplicable

--- parcheesi/move/action/BreakBlockade.java
+++ parcheesi/move/action/BreakBlockade.java
@@ -7,16 +7,17 @@ import parcheesi.pawn.Pawn;
 import parcheesi.die.Die;
 import parcheesi.Board;
 
-public class BreakBlockade extends MoveForward {
+public class BreakBlockade extends MoveForward implements Action {
 	public boolean isApplicable(Class<? extends Move> MoveClass, Die die, Pawn pawn, Board board) {
 		if (!super.isApplicable(MoveClass, die, pawn, board)) {
 			return false;
 		}
 
 		List<Pawn> pawns = board.getPawnsAtCoordinate(board.getPawnCoordinate(pawn));
-		boolean currentlyInBlockade = pawns.stream().anyMatch(p -> pawn.color.equals(p.color));
-
-		return currentlyInBlockade;
+		return pawns
+			.stream()
+			.filter(p -> !pawn.equals(p))
+			.anyMatch(p -> pawn.color.equals(p.color));
 	}
 
 	public boolean apply(Die die, Pawn pawn, Board board) {
@@ -24,6 +25,47 @@ public class BreakBlockade extends MoveForward {
 		return false;
 	}
 
-	public static void main(String[] args) {
+	// NOTE: singleton.
+	public static BreakBlockade action = new BreakBlockade();
+
+	public static void main(String[] args) throws parcheesi.die.Die.InvalidDieException {
+		new BreakBlockadeTester();
+	}
+
+	private static class BreakBlockadeTester extends parcheesi.test.Tester {
+		public BreakBlockadeTester() throws parcheesi.die.Die.InvalidDieException {
+			Board board = new Board();
+			Pawn pawn   = new Pawn(0, parcheesi.Color.forPlayer(0).getColorName());
+			Pawn pawn2  = new Pawn(1, parcheesi.Color.forPlayer(0).getColorName());
+			Die die     = new parcheesi.die.NormalDie(3);
+
+			board.addPawn(pawn);
+
+			check(
+				!action.isApplicable(parcheesi.move.MoveMain.class, die, pawn, board),
+				"BreakBlockade is not applicable on a board without any blockades"
+			);
+
+			board.addPawn(pawn2);
+
+			check(
+				action.isApplicable(parcheesi.move.MoveMain.class, die, pawn, board),
+				"BreakBlockade is applicable on a board where pawns with same color share space"
+			);
+
+			// TODO: test breaking the blockade (apply, not just isApplicable)
+			board.movePawnForward(pawn2, die.getValue());
+
+			Pawn otherPawn = new Pawn(0, parcheesi.Color.forPlayer(1).getColorName());
+			board.addPawn(otherPawn);
+			board.movePawnForward(otherPawn, parcheesi.Parameters.Board.pawnMainRingDistance);
+
+			check(
+				!action.isApplicable(parcheesi.move.MoveMain.class, die, pawn, board),
+				"BreakBlockade is not applicable on a board where pawns with differing colors share space"
+			);
+
+			summarize();
+		}
 	}
 }
--- parcheesi/move/action/FormBlockade.java
+++ parcheesi/move/action/FormBlockade.java
@@ -2,23 +2,30 @@ package parcheesi.move.action;
 
 import java.util.List;
 
+import parcheesi.move.EnterPiece;
+import parcheesi.move.MoveMain;
 import parcheesi.move.Move;
 import parcheesi.pawn.Pawn;
 import parcheesi.die.Die;
 import parcheesi.Board;
 
-public class FormBlockade extends MoveForward {
+public class FormBlockade implements Action {
 	public boolean isApplicable(Class<? extends Move> MoveClass, Die die, Pawn pawn, Board board) {
-		if (!super.isApplicable(MoveClass, die, pawn, board)) {
+		Board testBoard = new Board(board);
+
+		if (MoveClass == EnterPiece.class) {
+			testBoard.addPawn(pawn);
+		} else if (MoveClass == MoveMain.class) {
+			testBoard.movePawnForward(pawn, die.getValue());
+		} else {
 			return false;
 		}
 
-		Board testBoard = new Board(board);
-		testBoard.movePawnForward(pawn, die.getValue());
 		List<Pawn> pawns = testBoard.getPawnsAtCoordinate(testBoard.getPawnCoordinate(pawn));
-		boolean pawnAtTargetHasSameColor = pawns.stream().anyMatch(p -> pawn.color.equals(p.color));
-
-		return pawnAtTargetHasSameColor;
+		return pawns
+			.stream()
+			.filter(p -> !pawn.equals(p))
+			.anyMatch(p -> pawn.color.equals(p.color));
 	}
 
 	public boolean apply(Die die, Pawn pawn, Board board) {
@@ -26,6 +33,54 @@ public class FormBlockade extends MoveForward {
 		return false;
 	}
 
-	public static void main(String[] args) {
+	// NOTE: singleton.
+	public static FormBlockade action = new FormBlockade();
+
+	public static void main(String[] args) throws Die.InvalidDieException {
+		new FormBlockadeTester();
+	}
+
+	private static class FormBlockadeTester extends parcheesi.test.Tester {
+		public FormBlockadeTester() throws Die.InvalidDieException {
+			Board board = new Board();
+			Pawn pawn = new Pawn(0, parcheesi.Color.forPlayer(0).getColorName());
+			Die die = new parcheesi.die.NormalDie(5);
+
+			board.addPawn(pawn);
+
+			check(
+				!action.isApplicable(MoveMain.class, die, pawn, board),
+				"FormBlockade is not applicable on a board with only 1 pawn"
+			);
+
+			Pawn pawn2 = new Pawn(1, parcheesi.Color.forPlayer(0).getColorName());
+
+			check(
+				action.isApplicable(EnterPiece.class, die, pawn2, board),
+				"FormBlockade is applicable to EnterPiece when there is a pawn of same color on entry"
+			);
+			
+			board.addPawn(pawn2);
+			board.movePawnForward(pawn, die.getValue());
+
+			check(
+				action.isApplicable(MoveMain.class, die, pawn2, board),
+				"FormBlockade is applicable to MoveMain when moving to position having a pawn of same color"
+			);
+
+			check(
+				!action.isApplicable(EnterPiece.class, die, pawn2, board),
+				"FormBlockade is not applicable to EnterPiece move on a pawn that already entered"
+			);
+
+			pawn.color = parcheesi.Color.forPlayer(1).getColorName();
+
+			check(
+				!action.isApplicable(MoveMain.class, die, pawn2, board),
+				"FormBlockade is not applicable to MoveMain when moving to a pawn of a different color"
+			);
+
+			summarize();
+		}
 	}
 }
